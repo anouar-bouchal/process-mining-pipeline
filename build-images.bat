607862a42ec59b09278b6942c9223d76d8e08865
Anouar BOUCHAL <bouchal.anouar@gmail.com>
2022-05-12 02:24:29 +0100

    Build images with up to date apache archive links

---
+++ build-images.bat
@@ -0,0 +1,24 @@
+set SPARK_VERSION = 2.4.7
+set HADOOP_VERSION = 2.7
+
+echo '\n==========================================================='
+echo '\n# Building Spark Images'
+echo '\n==========================================================='
+
+docker build -t spark-base ./spark/images/spark-base/ --build-arg SPARK_VERSION=2.4.7 --build-arg HADOOP_VERSION=2.7
+echo '\n# Building spark-master'
+docker build -t spark-master ./spark/images/spark-master/
+echo '\n# Building spark-worker'
+docker build -t spark-worker ./spark/images/spark-worker/
+echo '\n# Building spark-submit'
+docker build -t spark-submit ./spark/images/spark-submit/
+
+echo '\n==========================================================='
+echo '\n# Building Kafka Images'
+echo '\n==========================================================='
+
+docker build -t kafka-base ./kafka/kafka-base/
+echo '\n# Building kafka-server'
+docker build -t kafak-server ./kafka/kafka-server/
+echo '\n# Building kafka-zookeeper'
+docker build -t kafka-zookeeper ./kafka/kafka-zookeeper/
---
+++ spark/spark-apps/transformer.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+
+def handle_rdd(rdd):
+    if not rdd.isEmpty():
+        global ss       
+        df = None
+
+        try:
+            df = ss.createDataFrame(rdd, schema=["id", "lat", "lng", "aff_date"])
+        except Exception as e:
+            pass
+
+        print("########################################################")
+        print("########################################################")
+        print("\n")
+        print("\n")
+        print("\n")
+
+        print(df)
+        # df.write.saveAsTable(name='default.covid', format='hive', mode='append')
+
+        print("\n")
+        print("\n")
+        print("\n")
+        print("########################################################")
+        print("########################################################")
+
+
+sc = SparkContext(appName="Something")
+ssc = StreamingContext(sc, 5)
+
+ss = (
+    SparkSession.builder.appName("covid streaming")
+    .config("spark.sql.warehouse.dir", "/user/hive/warehouse")
+    .config("hive.metastore.uris", "thrift://hive-metastore:9083")
+    .enableHiveSupport()
+    .getOrCreate()
+)
+
+
+# ss.sparkContext.setLogLevel('WARN')
+
+ks = KafkaUtils.createDirectStream(
+    ssc, ["covid-new-cases"], {"metadata.broker.list": "kafka-server:9092"}
+)
+
+lines = ks.map(lambda x: x[1])
+
+transform = lines.map(lambda data: (data.split(";")))
+
+transform.foreachRDD(handle_rdd)
+
+ssc.start()
+ssc.awaitTermination()
