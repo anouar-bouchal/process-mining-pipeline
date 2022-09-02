fa4df50204bcea55f547ef5bd9d41b0bffffb985
Anouar BOUCHAL <bouchal.anouar@gmail.com>
2022-09-02 18:07:49 +0100

    Add batch build script

--- build-images.bat
+++ build-images.bat
@@ -1,19 +1,31 @@
-echo '\n==========================================================='
-echo '\n# Building Spark Images'
-echo '\n==========================================================='
+@echo off
+echo     ===========================================================
+echo:
+echo               # Building Spark Images
+echo:
+echo     ===========================================================
+echo:
 
 docker build -t spark-base ./spark/images/spark-base/
-echo '\n# Building spark-master'
+echo    ## Building spark-master
+echo:
 docker build -t spark-master ./spark/images/spark-master/
-echo '\n# Building spark-worker'
+echo    ## Building spark-worker
+echo:
 docker build -t spark-worker ./spark/images/spark-worker/
 
-echo '\n==========================================================='
-echo '\n# Building Kafka Images'
-echo '\n==========================================================='
+echo:
+echo    ===========================================================
+echo:
+echo               # Building Kafka Images
+echo:
+echo    ===========================================================
+echo:
 
 docker build -t kafka-base ./kafka/kafka-base/
-echo '\n# Building kafka-server'
+echo    ## Building kafka-server
+echo:
 docker build -t kafka-server ./kafka/kafka-server/
-echo '\n# Building kafka-zookeeper'
+echo    ## Building kafka-zookeeper
+echo:
 docker build -t kafka-zookeeper ./kafka/kafka-zookeeper/
